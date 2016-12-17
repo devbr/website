b2ba130a15c080c50c203f2daea0eb50a4b6433f
Bill Rocha <ahcordesign@gmail.com>
2016-12-17 14:49:21 -0200

    Blue Spring

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <?php
 include '.php/Config/App.php';
+
+//Delete this ...
+echo '<div style="width:300px;margin:10% auto;text-align:center"><h1 style="font-size:7em;font-family:monospace">Hello World!</h1>Developed by <a href="https://github.com/devbr/website">https://github.com/devbr/website</a></div>';
