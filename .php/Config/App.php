d7c6afbcddc86417b2d959a01a597b538fc8400b
Bill Rocha <ahcordesign@gmail.com>
2017-01-01 14:50:32 -0200

    Orange Summer

--- .php/Config/App.php
+++ .php/Config/App.php
@@ -1,35 +1,15 @@
 <?php
 /**
- * PHP Base
- * @copyright   Bill Rocha - http://google.com/+BillRocha
- * @license     MIT
- * @author      Bill Rocha - prbr@ymail.com
- * @version     0.0.1
- * @package     Config
- * @access      public
- * @since       0.3.0
+ * Config\App
+ * PHP version 7
  *
- * The MIT License
- *
- * Copyright 2015 http://google.com/+BillRocha.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * @category  Config
+ * @package   App
+ * @author    Bill Rocha <prbr@ymail.com>
+ * @copyright 2016 Bill Rocha <http://google.com/+BillRocha>
+ * @license   <https://opensource.org/licenses/MIT> MIT
+ * @version   GIT: 0.0.2
+ * @link      http://paulorocha.tk/github/devbr
  */
 
 // Defaults
@@ -65,4 +45,4 @@ spl_autoload_register(function($class) {
 if(file_exists(_APP.'Composer/autoload.php')) include _APP.'Composer/autoload.php';
 
 //Onm Cli mode
-if(php_sapi_name() === 'cli') return new Resource\Core($argv);
+if(php_sapi_name() === 'cli') return new Resource\CLTool($argv);
