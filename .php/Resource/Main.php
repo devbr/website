506ca7289254942e620fbf89196f1fbd788df944
Bill Rocha <ahcordesign@gmail.com>
2017-04-14 02:48:39 -0300

    Bug in index
    Correção da verificação do arquivo home.html

--- .php/Resource/Main.php
+++ .php/Resource/Main.php
@@ -75,7 +75,7 @@ class Main
      */
     function index()
     {
-        if (!file_exists(_HTML.'nopage.html')) {
+        if (!file_exists(_HTML.'home.html')) {
             exit('<style>h1{font-size:2em;margin:0;padding:5px 0}div{position:absolute;top:50%;left:50%;width:50%;margin:-145px 0 0 -25%;text-align:center}</style><a href="https://github.com/devbr/website"><div><h1>Hello World!</h1>More info in Github.</div></a>');
         }
         //Configuration of style & script
